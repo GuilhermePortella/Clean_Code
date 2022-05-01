fc5eb28891c62787c7a52a72a7e48d1265d9b8b8
guilhermeportella <guilhermeportella2@gmail.com>
2022-04-30 23:44:46 -0300

    Inicializando o projeto

---
+++ src/clean/code/CleanCode.java
@@ -0,0 +1,16 @@
+/*
+ * Honestidade em pequenas coisas não é uma coisa pequena
+ */
+package clean.code;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class CleanCode {
+
+    public static void main(String[] args) {
+        System.out.print("Exemplos de um codigo ruim e um bom codigo\n");
+    }
+    
+}
