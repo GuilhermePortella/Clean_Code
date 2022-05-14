4d4d0484f383947c5e12cdd05b5de590cf88e8bf
guilhermeportella <guilhermeportella2@gmail.com>
2022-05-14 20:53:44 -0300

    Exemplo de codigo ruim

--- src/clean/code/CleanCode.java
+++ src/clean/code/CleanCode.java
@@ -3,14 +3,33 @@
  */
 package clean.code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Guilherme
  */
 public class CleanCode {
+    
+    int d;
+    
+    int elapsedTimeInDays;
+    int daysSinceCreation;
+    int daysSinceModifacation;
+    int fileAgeInDays;
 
     public static void main(String[] args) {
         System.out.print("Exemplos de um codigo ruim e um bom codigo\n");
+    }
+    
+    public List<int[]> getThem() {
+        List<int[]> lis1 = new ArrayList<int[]>();
+        for(int [] x : theList){
+            if (x[0]==4)
+                lis1.add(x);
+        }
+        return lis1;
     }
     
 }
